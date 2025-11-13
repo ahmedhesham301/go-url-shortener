855e11c2000ec4de51b568844c4e3b0dddf414a5
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-11-13 17:44:43 +0200

    update the fetch url to be the url that the user connected to

--- frontend/app/src/components/UrlShortener.tsx
+++ frontend/app/src/components/UrlShortener.tsx
@@ -15,7 +15,7 @@ const UrlShortener: React.FC = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost/api/create', {
+      const response = await fetch(`${window.location.origin}/api/create`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
